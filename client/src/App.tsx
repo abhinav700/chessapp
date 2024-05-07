15cc53a99a46497d211c9d082e73e58d6ab66023
Abhinav Agarwal <abhinavagrawal700@gmail.com>
2024-05-07 14:42:54 +0530

    Added tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
     </div>
   );
 }
