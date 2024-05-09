c2efc491f5a732f8441dcccc166ac43bf09d6569
Abhinav Agarwal <abhinavagrawal700@gmail.com>
2024-05-09 15:59:03 +0530

    refactoring

---
+++ client/src/messages.ts
@@ -0,0 +1,5 @@
+export const INIT_GAME = "init_game";
+export const MAKE_MOVE = "make_move";
+export const GAME_OVER = "game_over";
+export const GAME_DRAW = "game_draw";
+export const GAME_STARTED = "game_started";
