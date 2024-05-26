342f6ad6d1f2ab0c97905489f96f325db0a29426
Abhinav Agarwal <abhinavagrawal700@gmail.com>
2024-05-26 13:18:38 +0530

    Added Notations

--- client/src/Components/ChessBoard.tsx
+++ client/src/Components/ChessBoard.tsx
@@ -1,7 +1,7 @@
 import { Chess, Color, PieceSymbol, Square } from "chess.js";
 import React, { useEffect, useState } from "react";
 import { MAKE_MOVE } from "../messages";
-import moveSelfAudio  from "../assets/sounds/move-self.mp3"
+import moveSelfAudio from "../assets/sounds/move-self.mp3";
 import { playSound } from "../utils/playSound";
 
 type ChessBoardProps = {
@@ -15,16 +15,11 @@ type ChessBoardProps = {
   setBoard: any;
 };
 
-
-console.log(moveSelfAudio)
+console.log(moveSelfAudio);
 const ChessBoard = ({ chess, board, setBoard, socket }: ChessBoardProps) => {
-  
   const [from, setFrom] = useState<Square | null>(null);
   const [to, setTo] = useState<Square | null>(null);
 
-  
-   
-  
   const onClickHandler = (
     e: React.MouseEvent<HTMLDivElement, MouseEvent>,
     squareRepresentation: Square | null
@@ -36,7 +31,7 @@ const ChessBoard = ({ chess, board, setBoard, socket }: ChessBoardProps) => {
       setTo((to) => squareRepresentation);
       chess.move({ from: from, to: squareRepresentation });
       setBoard(chess.board());
-      playSound(moveSelfAudio)
+      playSound(moveSelfAudio);
       const mess = JSON.stringify({
         type: MAKE_MOVE,
         payload: {
@@ -51,21 +46,34 @@ const ChessBoard = ({ chess, board, setBoard, socket }: ChessBoardProps) => {
       setFrom(null);
     }
   };
-  const displayCharacter = (square: {
-    square: Square;
-    type: PieceSymbol;
-    color: Color;
-  } | null) => {
+  const displayCharacter = (
+    square: {
+      square: Square;
+      type: PieceSymbol;
+      color: Color;
+    } | null
+  ) => {
     {
-      const piece = `${square?.color}${square?.type}`
-      return square ? <img className="w-24" src={`/images/${piece}.png`} /> : "";
+      const piece = `${square?.color}${square?.type}`;
+      return (
+        <>
+          {square ? (
+            <img
+              className="w-12 xl:w-20 lg:w-16 w-12 mx-auto my-auto"
+              src={`/images/${piece}.png`}
+            />
+          ) : (
+            ""
+          )}
+        </>
+      );
     }
   };
   return (
     <div className="text-white">
       {board.map((row, i) => {
         return (
-          <div key={i} className="flex">
+          <div key={i} className="flex 	">
             {row.map((square, j) => {
               const squareRepresentation = (String.fromCharCode(97 + (j % 8)) +
                 "" +
@@ -73,12 +81,21 @@ const ChessBoard = ({ chess, board, setBoard, socket }: ChessBoardProps) => {
               return (
                 <div
                   key={j}
-                  className={`w-24 h-24 ${
+                  className={`lg:w-24 lg:h-24 ${
                     (i + j) % 2 == 0 ? "bg-green-800" : "bg-white text-black"
-                  } flex items-center justify-center`}
+                  } flex w-20 h-20` }
                   onClick={(e) => onClickHandler(e, squareRepresentation)}
                 >
+                  {j === 0 ? (
+                    <p className="relative mt-[5px] lg:left-2"> {8 - i} </p>
+                  ) : null}
+
                   {displayCharacter(square)}
+                  {i === 7 ? (
+                    <p className="relative mt-[55px] lg:mt-[65px]  right-2">
+                      {String.fromCharCode(97 + j)}
+                    </p>
+                  ) : null}
                 </div>
               );
             })}
