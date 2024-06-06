468ea91e678b70662ce98b73615cd2b6e4f81f35
Abhinav Agarwal <abhinavagrawal700@gmail.com>
2024-06-06 12:08:09 +0530

    Pawn promotion logic

--- client/src/Components/ChessBoard.tsx
+++ client/src/Components/ChessBoard.tsx
@@ -1,4 +1,4 @@
-import { Chess, Color, PieceSymbol, Square } from "chess.js";
+import { Chess, Color, Move, PieceSymbol, Square } from "chess.js";
 import React, { useEffect, useState } from "react";
 import { MAKE_MOVE } from "../messages";
 import moveSelfAudio from "../assets/sounds/move-self.mp3";
@@ -23,10 +23,48 @@ console.log(moveSelfAudio);
 const ChessBoard = ({ chess, board, setBoard, socket, myColor }: ChessBoardProps) => {
   const [from, setFrom] = useState<Square | null>(null);
   const [isFlipped, setIsFlipped] = useRecoilState(isBoardFlippedAtom);
+  const [showPromotionModal, setShowPromotionModal] = useState(false)
+  const [to, setTo] = useState<Square | null>(null)
+  const [promotingTo, setPromotingTo] = useState<null | string>(null)
+  const promotionOptions = ['q', 'r', 'b', 'n']
+  const promotionOptionsImages = promotionOptions.map((type) => {
+    const color = myColor === "black" ? "b" : 'w';
+    const piece = `${color}${type}`;
+    return (
+      <img
+        className="lg:w-14 w-11 my-3 mx-1 cursor-pointer hover:opacity-70"
+        src={`/images/${piece}.png`}
+        onClick={(e)=>{setPromotingTo(promotingTo =>type)}}
+      />
+    )
+  })
   useEffect(() => {
     setIsFlipped(isFlipped => myColor === "black");
   }, [myColor])
-  const onClickHandler = (
+
+  useEffect(() => {
+    if (promotingTo) {
+      const move = { from: from!, to: to!, promotion: promotingTo! };
+      updateBoardAfterMove(move)
+      setShowPromotionModal(showPromotionModal => false);
+    }
+  }, [promotingTo])
+
+
+  const updateBoardAfterMove = (move: { from: Square; to: Square, promotion?: string }) => {
+    chess.move(move);
+    const mess = JSON.stringify({
+      type: MAKE_MOVE,
+      payload: {
+        move
+      },
+    });
+    setBoard(chess.board());
+    playSound(moveSelfAudio);
+    socket.send(mess);
+    setFrom(null);
+  }
+  const makeMoveHandler = (
     e: React.MouseEvent<HTMLDivElement, MouseEvent>,
     squareRepresentation: Square | null
   ) => {
@@ -34,25 +72,13 @@ const ChessBoard = ({ chess, board, setBoard, socket, myColor }: ChessBoardProps
       setFrom((from) => squareRepresentation);
     } else {
       let move;
-      if(isPromoting(squareRepresentation!, from, chess)){
-        move =  {from, to: squareRepresentation,promotion:"q" }
-        chess.move(move);
+      setTo(to => squareRepresentation)
+      if (!isPromoting(squareRepresentation!, from, chess)) {
+        move = { from: from!, to: squareRepresentation! };
+        updateBoardAfterMove(move);
+
       }
-      else{
-        move = {from, to:squareRepresentation};
-        chess.move(move);
-      }
-      setBoard(chess.board());
-      playSound(moveSelfAudio);
-      const mess = JSON.stringify({
-        type: MAKE_MOVE,
-        payload: {
-          move
-        },
-      });
-      console.log(mess);
-      socket.send(mess);
-      setFrom(null);
+ 
     }
   };
 
@@ -64,7 +90,7 @@ const ChessBoard = ({ chess, board, setBoard, socket, myColor }: ChessBoardProps
       return false;
     if (to[1] != '1' && to[1] != '8')
       return false;
-
+    setShowPromotionModal(showPromotionModal => true);
     return true;
 
   }
@@ -93,47 +119,53 @@ const ChessBoard = ({ chess, board, setBoard, socket, myColor }: ChessBoardProps
   };
   return (
     <>
+{
+  showPromotionModal&&<div className="flex flex-col items-center w-[240px] h-[200px] bg-slate-500 relative rounded-lg top-[155px] lg:top-[160px] left-[125px] lg:left-[135px]">
+        <h1 className="text-xl"> Pawn Promotion</h1>
+        <div className="flex flex-wrap w-[140px] justify-center items-center">
+          {promotionOptionsImages}
+        </div>
+      </div>
+}
       
-    <div className="text-white">
-      <h1>{myColor}</h1>
-    z
-      
-      {(isFlipped ? board.slice().reverse() : board).map((row, i) => {
-        i = isFlipped ? i + 1 : 8 - i
-        return (
-          <div key={i} className="flex 	">
-            {(isFlipped ? row.slice().reverse() : row).map((square, j) => {
-              j = isFlipped ? 8 - j : j + 1
-              const squareRepresentation = (String.fromCharCode(97 + j - 1) +
-                "" +
-                (i)) as Square;
-              console.log(squareRepresentation)
-              return (
-                <div
-                  key={j}
-                  className={`text-black ${(i + j) % 2 == 0 ? "bg-green-800" : "bg-white"} flex w-16 h-16`}
-                  onClick={(e) => onClickHandler(e, squareRepresentation)}
-                >
+      <div className={`text-white ${showPromotionModal ? 'mt-[-200px]' : ""}`}>
+        <h1>{myColor}</h1>
+        {(isFlipped ? board.slice().reverse() : board).map((row, i) => {
+          i = isFlipped ? i + 1 : 8 - i
+          return (
+            <div key={i} className="flex 	">
+              {(isFlipped ? row.slice().reverse() : row).map((square, j) => {
+                j = isFlipped ? 8 - j : j + 1
+                const squareRepresentation = (String.fromCharCode(97 + j - 1) +
+                  "" +
+                  (i)) as Square;
+                console.log(squareRepresentation)
+                return (
+                  <div
+                    key={j}
+                    className={`text-black ${(i + j) % 2 == 0 ? "bg-green-800" : "bg-white"} flex w-16 h-16`}
+                    onClick={(e) => makeMoveHandler(e, squareRepresentation)}
+                  >
 
 
-                  {((!isFlipped && j == 1) || (isFlipped && j == 8)) ? (
-                    <p className="relative  w-[1px] "> {i} </p>
-                  ) : null}
+                    {((!isFlipped && j == 1) || (isFlipped && j == 8)) ? (
+                      <p className="relative  w-[1px] "> {i} </p>
+                    ) : null}
 
-                  {displayPiece(square)}
-                  {((!isFlipped && i == 1) || (isFlipped && i == 8)) ? (
-                    <p className="relative mt-[45px] w-[2px] right-2">
-                      {
-                        String.fromCharCode(97 + j - 1)}
-                    </p>
-                  ) : null}
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
-    </div>
+                    {displayPiece(square)}
+                    {((!isFlipped && i == 1) || (isFlipped && i == 8)) ? (
+                      <p className="relative mt-[45px] w-[2px] right-2">
+                        {
+                          String.fromCharCode(97 + j - 1)}
+                      </p>
+                    ) : null}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </>
 
   );
