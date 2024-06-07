cef1aedaead2c08161c8a15cc17d5996cf98ec3f
Abhinav Agarwal <abhinavagrawal700@gmail.com>
2024-06-07 12:30:36 +0530

    Code clean up

--- client/src/screens/LandingPage.tsx
+++ client/src/screens/LandingPage.tsx
@@ -12,28 +12,19 @@ const LandingPage = () => {
       <div className="max-w-[1200px] mt-2 gap-4 grid grid-cols-2 md:grid-cols-2">
         <div className=" col-span-1 flex justify-center items-center">
           {" "}
-          {/* Center both horizontally and vertically */}
           <img
             src={"/images/chessboard.jpeg"}
             className="w-[500px] md:w-[700px]"
           />{" "}
-          {/* Set image height */}
         </div>
         <div className=" col-span-1 flex flex-col items-center justify-center md:h-[350px]">
           <h1 className="text-6xl text-[#afbdb1] text-center px-4 py-2 font-bold mb-8">
             Play Chess online on the #1 website
           </h1>{" "}
-          {/* Center text, adjust padding, add margin */}
           <Button style = {"px-8 py-2 font-bold bg-green-700 rounded text-white hover:bg-green-600 cursor-pointer col-span-2"} onClick={onPlayOnlineClick}>
             Play Online
           </Button>
-          {/* <button
-            onClick={onPlayOnlineClick}
-            className="bg-black p-3 text-white rounded-md shadow-md hover:shadow-lg hover:bg-[#1a1919]"
-          >
-            Play online
-          </button>{" "} */}
-          {/* Add shadow and hover effect */}
+        
         </div>
       </div>
     </div>
